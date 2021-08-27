7997f28114564e2bd65aefc2afa221f01fa789fc
Sahaja Verma <78591386+SahajaVerma@users.noreply.github.com>
2021-08-28 01:11:46 +0530

    Update 2-Line-Calculator.py

--- Python/2-Line-Calculator.py
+++ Python/2-Line-Calculator.py
@@ -1,2 +1,2 @@
-x=input("Write a Mathematical Expression Like 2+2 or 6+6+2-5")
-print("Result : ",eval(n))
+x=input("Write a Mathematical Expression Like 2+2 or 6+6+2-5\n")
+print("Result : ",eval(x))
